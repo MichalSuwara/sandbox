cc9d5519379ba78c31df60a549f22d08ed45c5ae
michal.suw.20@gmail.com <7818_ARgo>
2019-04-27 20:43:16 +0200

    pierwszy

--- src/main/java/pl/suwara/sandbox/SandboxApplication.java
+++ src/main/java/pl/suwara/sandbox/SandboxApplication.java
@@ -3,10 +3,26 @@ package pl.suwara.sandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EntityScan("pl.suwara.sandbox.persistance.model")
+@EnableSwagger2
 @SpringBootApplication
 public class SandboxApplication {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SandboxApplication.class, args);
